3dd9bbf4e601fa5966e9ca79267b83ae3499f4f1
oleleiNET <115529700+oleleiNET@users.noreply.github.com>
2023-04-12 16:54:02 +0200

    footer update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
         <footer className={style.background}>
             <div className={style.foot}>
                 <p className={style.foot__item}>Copyright © ketozocha.pl 2023</p>
-                <a href='https://studio-www.com/' target='_blank' rel="noopener noreferrer" className={`${style.designedBy} ${style.link}`}>
+                <a href='https://alexa-web.com/' target='_blank' rel="noopener noreferrer" className={`${style.designedBy} ${style.link}`}>
                     <span className={`${style.text} ${style.textLeft}`}>Designed & Developed by</span>
                     <img className={style.logo} src={logo} alt='logo' />
                     <span className={`${style.text} ${style.textRight}`}>Alexa-Web.com</span>
